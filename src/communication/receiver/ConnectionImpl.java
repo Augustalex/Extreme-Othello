80c383080c02d97e368fdbd79fe1474fc1938fe1
S132063 <S132063@PC966813.ida.utb.hb.se>
2016-10-27 20:19:39 +0200

    Ja förstör världen

---
+++ src/communication/receiver/ConnectionImpl.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package communication.receiver;
+
+import communication.sender.Package;
+import java.net.Socket;
+
+
+public class ConnectionImpl implements Connection {
+
+    @Override
+    public void connect() throws Exception {
+        //Socket clientSocket = new Socket("localhost", 45000);
+        
+    }
+
+    @Override
+    public Package receive() {
+        
+    }
+
+    @Override
+    public void close() {
+        
+    }
+    
+}
